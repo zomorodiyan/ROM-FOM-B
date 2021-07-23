ad3cd013d3b6e62bd42c8621ca41177b15c3f4b1
zo <mehrdadzomorodiyan@gmail.com>
2021-07-23 13:09:04 -0500

    first window_size steps of the romfom

--- romfom.py
+++ romfom.py
@@ -2,6 +2,7 @@ import numpy as np
 from silence_tensorflow import silence_tensorflow
 silence_tensorflow()
 from tensorflow.keras.models import load_model
+from tools import initial, podproj_svd, RK3, BoussRHS
 
 def RK3t(rhs,nx,ny,dx,dy,Re,Pr,Ri,w,s,t,dt):
 # time integration using third-order Runge Kutta method
@@ -58,7 +59,7 @@ y = np.linspace(0.0,ly,ny+1)
 X, Y = np.meshgrid(x, y, indexing='ij')
 
 # run a coarse fom, get psi omega theta for each step and each mesh
-# (run fom.py)
+# (run fom.py get fom_nx'x'ny)
 
 # run svd on psi phi theta, get phi_psi phi_omega phi_theta for each mesh
 #   and alpha beta for number of modes and each time step
@@ -67,29 +68,51 @@ X, Y = np.meshgrid(x, y, indexing='ij')
 # run lstm on alpha beta, get model
 # (run lstm.py)
 
-# set the initial condition for psi omega theta
-n=0; time=0
+# Inputs (move to yml)
+window_size = 3
+
+lx = 8; ly = 1
+nx = 128; ny = int(nx/8)
+Re = 1e4; Ri = 4; Pr = 1
+Tm = 8; dt = 5e-4; nt = int(np.round(Tm/dt))
+ns = 800; freq = int(nt/ns)
+nr = 50
+
+alpha = np.zeros([ns+1,nr])
+beta = np.zeros([ns+1,nr])
+xtest = np.zeros((ns-window_size, window_size, 2*nr))
+filename = './results/pod_'+ str(nx) + 'x' + str(ny) + '.npz'
+data = np.load(filename)
+w_mean = data['wm']; Phiw = data['Phiw']
+t_mean = data['tm']; Phit = data['Phit']
+
+n=0; time=0;
 w,s,t = initial(nx,ny)
+w_1d = w.reshape([-1,])
+w_spread_1d = w_1d - w_mean
+alpha[0,:] = podproj_svd(w_spread_1d,Phiw)
+t_1d = t.reshape([-1,])
+t_spread_1d = t_1d - t_mean
+beta[0,:] = podproj_svd(t_spread_1d,Phit)
 
 # for the first window_size steps
 #   1-run fom, get psi omega theta for each time step
 #   2-run phi_psi phi_omega phi_theta projection on psi omega theta,
 #     get alpha beta for each time step
 
-#calculate wm
-tmp = w.reshape([-1,])-wm
-alpha[0,:] = PODproj_svd(tmp,Phiw)
 #calculate tm
-tmp = t.reshape([-1,])-tm
-alpha[0,:] = PODproj_svd(tmp,Phiw)
-for i in range(1,lookback):
+for i in range(1, window_size):
+    time = time+dt
     w,s,t = RK3(BoussRHS,nx,ny,dx,dy,Re,Pr,Ri,w,s,t,dt)
-    tmp = w.reshape([-1,])-wm
-    alpha[i,:] = PODproj_svd(tmp,Phiw)
-    tmp = t.reshape([-1,])-tm
-    beta[i,:] = PODproj_svd(tmp,Phit)
-#concatenate alpha and beta
-#make xtest[0,0,:]
+    w_1d = w.reshape([-1,])
+    w_spread_1d = w_1d - w_mean
+    alpha[i,:] = podproj_svd(w_spread_1d,Phiw)
+    t_1d = t.reshape([-1,])
+    t_spread_1d = t_1d - t_mean
+    beta[i,:] = podproj_svd(t_spread_1d,Phit)
+    xtest[0,:,:] = np.concatenate((alpha, beta), axis=1)[0:window_size,:] # axes 0:snapshots 1:states
+
+#for i in range(window_size, nt):
 
 # for the rest of the steps
 #   3-run model on a window of alpha beta, get alpha_new (rom/ml)
